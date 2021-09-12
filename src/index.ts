56174a23e0414aa349571f7f181c994c29fb51d7
Oli Nelson <oliver.nelson@nirovision.com>
2021-09-12 11:04:44 +1000

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import * as core from '@actions/core'
+import {genMarkdown} from './genMarkdown'
 
 async function run(): Promise<void> {
   try {
-    const NOTION_API_KEY = core.getInput('NOTION_API_KEY', { required: true });
-    const NOTION_DATABASE_ID = core.getInput('NOTION_DATABASE_ID', { required: true });
-    const ms: string = core.getInput('milliseconds')
-    core.
+    const NOTION_API_KEY = core.getInput('NOTION_API_KEY', {required: true})
+    const NOTION_DATABASE_ID = core.getInput('NOTION_DATABASE_ID', {
+      required: true
+    })
+    await genMarkdown(NOTION_API_KEY, NOTION_DATABASE_ID)
+    return
   } catch (error) {
     core.setFailed(error.message)
   }
